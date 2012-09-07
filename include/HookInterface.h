37f8044c8c1960585f12b6eebb08835bb8e83819
Gillis Van Ginderachter <supergillis@gmail.com>
2012-09-07 16:35:35 +0200

    Every subproject now consists of a src folder and an optional include folder.

---
+++ include/HookInterface.h
@@ -0,0 +1,55 @@
+/* Copyright (c) 2012 Gillis Van Ginderachter <supergillis@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HOOKINTERFACE_H
+#define HOOKINTERFACE_H
+
+#include <QtGlobal>
+#include <QByteArray>
+
+#include <SettingsInterface.h>
+#include <SenderInterface.h>
+#include <PacketInterface.h>
+#include <PacketBuilderInterface.h>
+#include <PluginManagerInterface.h>
+#include <ProxyInterface.h>
+#include <ReceiverInterface.h>
+#include <ReadOnlyProxyInterface.h>
+#include <UIManagerInterface.h>
+
+class HookInterface {
+public:
+	virtual ~HookInterface() {}
+
+	virtual SettingsInterface* settings() = 0;
+	virtual SenderInterface* sender() = 0;
+    virtual ReceiverInterface* receiver() = 0;
+    virtual PluginManagerInterface* plugins() = 0;
+    virtual UIManagerInterface* ui() = 0;
+
+    virtual void addOutgoingProxy(quint8, ProxyInterface*) = 0;
+    virtual void removeOutgoingProxy(quint8, ProxyInterface*) = 0;
+
+    virtual void addOutgoingReadOnlyProxy(quint8, ReadOnlyProxyInterface*) = 0;
+    virtual void removeOutgoingReadOnlyProxy(quint8, ReadOnlyProxyInterface*) = 0;
+
+    virtual void addIncomingReadOnlyProxy(quint8, ReadOnlyProxyInterface*) = 0;
+    virtual void removeIncomingReadOnlyProxy(quint8, ReadOnlyProxyInterface*) = 0;
+
+    virtual PacketBuilderInterface* createPacket() const = 0;
+    virtual PacketBuilderInterface* createPacket(quint16 length) const = 0;
+};
+
+#endif
---
+++ include/ReceiverInterface.h
@@ -0,0 +1,29 @@
+/* Copyright (c) 2012 Gillis Van Ginderachter <supergillis@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef RECEIVERINTERFACE_H
+#define RECEIVERINTERFACE_H
+
+#include <QByteArray>
+
+class ReceiverInterface {
+public:
+	virtual ~ReceiverInterface() {}
+
+    virtual bool receiveOutgoingMessage(const QByteArray&) = 0;
+    virtual void receiveIncomingMessage(const QByteArray&) = 0;
+};
+
+#endif
